4f2d40c49bee0c01c77a789ccf9a67caccdbb977
Florian Rohm <rohm.florian@googlemail.com>
2015-12-10 13:11:22 +0100

    conversion works

--- code/lidDrivenCavity.py
+++ code/lidDrivenCavity.py
@@ -132,7 +132,6 @@ def collide(fin, omega, u):
     print amax(fin[7, :, :])
     print amax(fin[8, :, :])
 
-    print "\ncollide"
     # central moments
     ux = u[0, :, :]
     uy = u[1, :, :]
@@ -143,35 +142,35 @@ def collide(fin, omega, u):
 
     # c_{i beta}
     # c_{-1 beta}
-    c__n1_0 = fin[7, :, :] + fin[4, :, :] + fin[8, :, :] # f-1-1 + f-10 + f-11
-    c__n1_1 = (-1-uy)*fin[7, :, :] - uy*fin[4, :, :] + (1-uy)*fin[8, :, :]
-    c__n1_2 = (-1-uy)*(-1-uy)*fin[7, :, :] + uy*uy*fin[4, :, :] + (1-uy)*(1-uy)*fin[8, :, :]
+    cDash_n1_0 =       fin[3, :, :] +                 fin[7, :, :] +               fin[6, :, :]
+    cDash_n1_1 =   -uy*fin[3, :, :] +         (-1-uy)*fin[7, :, :] +        (1-uy)*fin[6, :, :]
+    cDash_n1_2 = uy*uy*fin[3, :, :] + (-1-uy)*(-1-uy)*fin[7, :, :] + (1-uy)*(1-uy)*fin[6, :, :]
 
     # c_{0 beta}
-    c__0_0 = fin[3, :, :] + fin[0, :, :] + fin[1, :, :] # f0-1 + f00 + f01
-    c__0_1 = (-1-uy)*fin[3, :, :] - uy*fin[0, :, :] + (1-uy)*fin[1, :, :]
-    c__0_2 = (-1-uy)*(-1-uy)*fin[3, :, :] + uy*uy*fin[0, :, :] + (1-uy)*(1-uy)*fin[1, :, :]
+    cDash_0_0 =       fin[0, :, :] +                 fin[4, :, :] +               fin[2, :, :]
+    cDash_0_1 =   -uy*fin[0, :, :] +         (-1-uy)*fin[4, :, :] +        (1-uy)*fin[2, :, :]
+    cDash_0_2 = uy*uy*fin[0, :, :] + (-1-uy)*(-1-uy)*fin[4, :, :] + (1-uy)*(1-uy)*fin[2, :, :]
 
     # c_{1 beta}
-    c__1_0 = fin[6, :, :] + fin[2, :, :] + fin[5, :, :] # f1-1 + f10 + f11
-    c__1_1 = (-1-uy)*fin[6, :, :] - uy*fin[2, :, :] + (1-uy)*fin[5, :, :]
-    c__1_2 = (-1-uy)*(-1-uy)*fin[6, :, :] + uy*uy*fin[2, :, :] + (1-uy)*(1-uy)*fin[5, :, :]
+    cDash_1_0 =       fin[1, :, :] +                 fin[8, :, :] +               fin[5, :, :]
+    cDash_1_1 =   -uy*fin[1, :, :] +         (-1-uy)*fin[8, :, :] +        (1-uy)*fin[5, :, :]
+    cDash_1_2 = uy*uy*fin[1, :, :] + (-1-uy)*(-1-uy)*fin[8, :, :] + (1-uy)*(1-uy)*fin[5, :, :]
 
     # c{alpha beta}
     # c{alpha 0}
-    c_00 = c__n1_0 + c__0_0 + c__1_0
-    c_10 = (-1-ux)*c__n1_0 - ux*c__0_0 + (1-ux)*c__1_0
-    c_20 = (-1-ux)*(-1-ux)*c__n1_0 + ux*ux*c__0_0 + (1-ux)*(1-ux)*c__1_0
+    c_00 =                 cDash_n1_0 +       cDash_0_0 +               cDash_1_0
+    c_10 =         (-1-ux)*cDash_n1_0 -    ux*cDash_0_0 +        (1-ux)*cDash_1_0
+    c_20 = (-1-ux)*(-1-ux)*cDash_n1_0 + ux*ux*cDash_0_0 + (1-ux)*(1-ux)*cDash_1_0
 
     # c{alpha 1}
-    c_01 = c__n1_1 + c__0_1 + c__1_1
-    c_11 = (-1-ux)*c__n1_1 - ux*c__0_1 + (1-ux)*c__1_1
-    c_21 = (-1-ux)*(-1-ux)*c__n1_1 + ux*ux*c__0_1 + (1-ux)*(1-ux)*c__1_1
+    c_01 =                 cDash_n1_1 +       cDash_0_1 +               cDash_1_1
+    c_11 =         (-1-ux)*cDash_n1_1 -    ux*cDash_0_1 +        (1-ux)*cDash_1_1
+    c_21 = (-1-ux)*(-1-ux)*cDash_n1_1 + ux*ux*cDash_0_1 + (1-ux)*(1-ux)*cDash_1_1
 
     # c{alpha 2}
-    c_02 = c__n1_2 + c__0_2 + c__1_2
-    c_12 = (-1-ux)*c__n1_2 - ux*c__0_2 + (1-ux)*c__1_2
-    c_22 = (-1-ux)*(-1-ux)*c__n1_2 + ux*ux*c__0_2 + (1-ux)*(1-ux)*c__1_2
+    c_02 =                 cDash_n1_2 +       cDash_0_2 +               cDash_1_2
+    c_12 =         (-1-ux)*cDash_n1_2 -    ux*cDash_0_2 +        (1-ux)*cDash_1_2
+    c_22 = (-1-ux)*(-1-ux)*cDash_n1_2 + ux*ux*cDash_0_2 + (1-ux)*(1-ux)*cDash_1_2
 
     # only K22 differs from the central moments
     K_22 = c_22 - 2*c_11*c_11 - c_20*c_02
@@ -187,51 +186,74 @@ def collide(fin, omega, u):
     print amax(c_12)
     print amax(c_22)
 
+    #print "\ncollide"
     # collision
+    #c_00_post = c_00
+    #c_10_post = c_10
+    #c_01_post = c_01#
+
+    #c_11_post = (1-omega)*c_11
+
+    #c_20_post = 1/3 + 0.5*(1-omega)*(c_20 - c_02)
+    #c_02_post = 1/3 - 0.5*(1-omega)*(c_20 - c_02)
+
+    #c_21_post = 0
+    #c_12_post = 0
+
+    #K_22_post = 0
+
+    # no Collision
     c_00_post = c_00
     c_10_post = c_10
     c_01_post = c_01
+    c_11_post = c_11
+    c_20_post = c_20
+    c_02_post = c_02
 
-    c_11_post = (1-omega)*c_11
+    c_21_post = c_21
+    c_12_post = c_12
 
-    c_20_post = 1/3 + 0.5*(1-omega)*(c_20 - c_02)
-    c_02_post = 1/3 - 0.5*(1-omega)*(c_20 - c_02)
-
-    c_21_post = 0
-    c_12_post = 0
-
-    K_22_post = 0
+    K_22_post = K_22
 
     # Transformation to central moments
     c_22_post = K_22_post + 2*c_11_post*c_11_post + c_20_post*c_02_post
 
+
     # backward transformation
-    c__n1_0_post = -0.5*(ux*(1 - ux))*c_00_post - 0.5*(1-2*ux)*c_10_post + 0.5*c_20_post
-    c__n1_1_post = -0.5*(ux*(1 - ux))*c_01_post - 0.5*(1-2*ux)*c_11_post + 0.5*c_21_post
-    c__n1_2_post = -0.5*(ux*(1 - ux))*c_02_post - 0.5*(1-2*ux)*c_12_post + 0.5*c_22_post
+    # cDash_n1_0_post = -0.5*(ux*(1 - ux))*c_00_post - 0.5*(1-2*ux)*c_10_post + 0.5*c_20_post
+    # cDash_0_0_post  =          (1-ux*ux)*c_00_post -         2*ux*c_10_post -     c_20_post
+    # cDash_1_0_post  =  0.5*(ux*(1 + ux))*c_00_post + 0.5*(1+2*ux)*c_10_post + 0.5*c_20_post
 
-    c__0_0_post = (1-ux*ux)*c_00_post - 2*ux*c_10_post - c_20_post
-    c__0_1_post = (1-ux*ux)*c_01_post - 2*ux*c_11_post - c_21_post
-    c__0_2_post = (1-ux*ux)*c_02_post - 2*ux*c_12_post - c_22_post
+    cDash_n1_0_post = -0.5*(ux*(1 - ux))*c_00_post - 0.5*(1-2*ux)*c_10_post + 0.5*c_20_post
+    cDash_0_0_post  =          (1-ux*ux)*c_00_post -         2*ux*c_10_post -     c_20_post
+    cDash_1_0_post  =  0.5*(ux*(1 + ux))*c_00_post + 0.5*(1+2*ux)*c_10_post + 0.5*c_20_post
 
-    c__1_0_post = 0.5*(ux*(1 - ux))*c_00_post + 0.5*(1+2*ux)*c_10_post + 0.5*c_20_post
-    c__1_1_post = 0.5*(ux*(1 - ux))*c_01_post + 0.5*(1+2*ux)*c_11_post + 0.5*c_21_post
-    c__1_2_post = 0.5*(ux*(1 - ux))*c_02_post + 0.5*(1+2*ux)*c_12_post + 0.5*c_22_post
+    cDash_n1_1_post = -0.5*(ux*(1 - ux))*c_01_post - 0.5*(1-2*ux)*c_11_post + 0.5*c_21_post
+    cDash_0_1_post  =          (1-ux*ux)*c_01_post -         2*ux*c_11_post -     c_21_post
+    cDash_1_1_post  =  0.5*(ux*(1 + ux))*c_01_post + 0.5*(1+2*ux)*c_11_post + 0.5*c_21_post
+
+    cDash_n1_2_post = -0.5*(ux*(1 - ux))*c_02_post - 0.5*(1-2*ux)*c_12_post + 0.5*c_22_post
+    cDash_0_2_post  =          (1-ux*ux)*c_02_post -         2*ux*c_12_post -     c_22_post
+    cDash_1_2_post  =  0.5*(ux*(1 + ux))*c_02_post + 0.5*(1+2*ux)*c_12_post + 0.5*c_22_post
 
     # post collision distributions
-    f__n1_n1_post = -0.5*(uy*(1 - uy))*c__n1_0_post - 0.5*(1-2*uy)*c__0_0_post + 0.5*c__1_0_post
-    f__n1_0_post = -0.5*(uy*(1 - uy))*c__n1_1_post - 0.5*(1-2*uy)*c__0_1_post + 0.5*c__1_1_post
-    f__n1_1_post = -0.5*(uy*(1 - uy))*c__n1_2_post - 0.5*(1-2*uy)*c__0_2_post + 0.5*c__1_2_post
+    # f__i_n1_post = -0.5*(uy*(1 - uy))*cDash_i_0_post - 0.5*(1-2*uy)*cDash_i_1_post + 0.5*cDash_i_2_post
+    # f__i_0_post  =        (1 - uy*uy)*cDash_i_0_post -         2*uy*cDash_i_1_post -     cDash_i_2_post
+    # f__i_1_post  = -0.5*(uy*(1 + uy))*cDash_i_0_post + 0.5*(1+2*uy)*cDash_i_1_post + 0.5*cDash_i_2_post
 
-    f__0_n1_post = (1-uy*uy)*c__n1_0_post - 2*uy*c__0_0_post - c__1_0_post
-    f__0_0_post = (1-uy*uy)*c__n1_1_post - 2*uy*c__0_1_post - c__1_1_post
-    f__0_1_post = (1-uy*uy)*c__n1_2_post - 2*uy*c__0_2_post - c__1_2_post
+    f__n1_n1_post = -0.5*(uy*(1 - uy))*cDash_n1_0_post - 0.5*(1-2*uy)*cDash_n1_1_post + 0.5*cDash_n1_2_post
+    f__n1_0_post  =        (1 - uy*uy)*cDash_n1_0_post -         2*uy*cDash_n1_1_post -     cDash_n1_2_post
+    f__n1_1_post  = -0.5*(uy*(1 + uy))*cDash_n1_0_post + 0.5*(1+2*uy)*cDash_n1_1_post + 0.5*cDash_n1_2_post
 
-    f__1_n1_post = 0.5*(uy*(1 - uy))*c__n1_0_post + 0.5*(1+2*uy)*c__0_0_post + 0.5*c__1_0_post
-    f__1_0_post = 0.5*(uy*(1 - uy))*c__n1_1_post + 0.5*(1+2*uy)*c__0_1_post + 0.5*c__1_1_post
-    f__1_1_post = 0.5*(uy*(1 - uy))*c__n1_2_post + 0.5*(1+2*uy)*c__0_2_post + 0.5*c__1_2_post
+    f__0_n1_post = -0.5*(uy*(1 - uy))*cDash_0_0_post - 0.5*(1-2*uy)*cDash_0_1_post + 0.5*cDash_0_2_post
+    f__0_0_post  =        (1 - uy*uy)*cDash_0_0_post -         2*uy*cDash_0_1_post -     cDash_0_2_post
+    f__0_1_post  = -0.5*(uy*(1 + uy))*cDash_0_0_post + 0.5*(1+2*uy)*cDash_0_1_post + 0.5*cDash_0_2_post
 
-    print "\npost collision cumulants"
+    f__1_n1_post = -0.5*(uy*(1 - uy))*cDash_1_0_post - 0.5*(1-2*uy)*cDash_1_1_post + 0.5*cDash_1_2_post
+    f__1_0_post  =        (1 - uy*uy)*cDash_1_0_post -         2*uy*cDash_1_1_post -     cDash_1_2_post
+    f__1_1_post  = -0.5*(uy*(1 + uy))*cDash_1_0_post + 0.5*(1+2*uy)*cDash_1_1_post + 0.5*cDash_1_2_post
+
+    print "\npost collision distributions"
     print amax(f__0_0_post)
 
     print amax(f__1_0_post)
