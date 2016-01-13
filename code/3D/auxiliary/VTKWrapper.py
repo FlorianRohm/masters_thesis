f97504a6fcf49e6991d416c2d99f41d666b42296
Florian Rohm <rohm.florian@googlemail.com>
2016-01-13 10:22:36 +0100

    formatting

--- code/3D/auxiliary/VTKWrapper.py
+++ code/3D/auxiliary/VTKWrapper.py
@@ -4,5 +4,5 @@ def saveToVTK(rho, u, prefix, saveNumber, grid):
     name = "./" + prefix + "." + saveNumber
     reorderedU = (u[0], u[1], u[2])
     gridToVTK(name, grid[0], grid[1], grid[2],
-              pointData = {'velocity': reorderedU,  
+              pointData = {'velocity': reorderedU,
                            'pressure': rho})
