304d3c3975b04dfc2816042fa007a55a57af01a1
Florian Rohm <rohm.florian@googlemail.com>
2016-02-17 19:29:07 +0100

    bugfix, imported the wrong module

--- code/2D/auxiliary/transformations/transforms.py
+++ code/2D/auxiliary/transformations/transforms.py
@@ -2,7 +2,7 @@ from numpy import *
 
 from centralMomentsFromDistributions import centralMomentsFromDistributions
 from normalizedCumulantsFromCentralMoments import normalizedCumulantsFromCentralMoments
-from normalizedCumulantsFromCentralMoments import normalizedCumulantsFromCentralMoments
+from centralMomentsFromNormalizedCumulants import centralMomentsFromNormalizedCumulants
 from distributionsFromCentralMoments import distributionsFromCentralMoments
 
 def normalizedCumulantsFromDistributions(rho, u, fin):
@@ -12,6 +12,6 @@ def normalizedCumulantsFromDistributions(rho, u, fin):
 
 def distributionsFromNormalizedCumulants(rho, u, K_00, K_10, K_01, K_11, K_20, K_02, K_21, K_12, K_22):
     (c_00, c_10, c_01, c_11, c_20, c_02, c_21, c_12, c_22) = \
-    normalizedCumulantsFromCentralMoments (rho, u, K_00, K_10, K_01, K_11, K_20, K_02, K_21, K_12, K_22)
+    centralMomentsFromNormalizedCumulants (rho, K_00, K_10, K_01, K_11, K_20, K_02, K_21, K_12, K_22)
 
     return distributionsFromCentralMoments (u, c_00, c_10, c_01, c_11, c_20, c_02, c_21, c_12, c_22)
