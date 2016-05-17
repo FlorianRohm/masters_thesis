1174d5379908f4e71639e018b7273fbab80a5ad3
Florian Rohm <rohm.florian@googlemail.com>
2016-05-17 21:12:36 +0200

    start of schaeffer turek benchmark

---
+++ code/2D/schaeferTurek.py
@@ -0,0 +1,211 @@
+#!/usr/bin/python
+# Copyright (C) 2013 FlowKit Ltd, Lausanne, Switzerland
+# E-mail contact: contact@flowkit.com
+#
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License, either
+# version 3 of the License, or (at your option) any later version.
+#
+# Extended Comments and bugfixed by Florian Rohm, 2015
+
+#
+# 2D flow around a cylinder
+#
+# D2Q9 Stencil with enumeration
+#
+# 6   2   5
+#   \ | /
+# 3 - 0 - 1
+#   / | \
+# 7   4   8
+#
+
+from numpy import *
+from matplotlib import cm, pyplot
+from auxiliary.VTKWrapper import saveToVTK
+from auxiliary.collide import BGKCollide, cumulantCollide
+from auxiliary.stream import stream
+from auxiliary.LBMHelpers import clamp, getMacroValues, sumPopulations, equilibrium, noslip, iLeft, iCentV, iRight, iTop, iCentH, iBot
+from auxiliary.boundaryConditions import YuLeft
+from auxiliary.obstacle import obstacleAttack, drag, lift
+import sys, getopt
+import os
+
+###### Parsing ############################################################
+
+Re = 100
+size = 10
+try:
+  opts, args = getopt.getopt(sys.argv[1:],"hr:s:",["re=","size="])
+except getopt.GetoptError:
+  print 'test.py -i <inputfile> -o <outputfile>'
+  sys.exit(2)
+  for opt, arg in opts:
+    if opt == '-h':
+        print 'test.py -r <reynolds number, default 100> -s <size of the sphere, divisible by 10, default 10>'
+        sys.exit()
+    elif opt in ("-r" "--re"):
+        Re = int(float(arg))
+    elif opt in ("-s", "--size"):
+        size = int(float(arg))
+print 'Reynolds number is', Re
+print 'Size of sphere is', size
+factor = size/10
+
+
+###### Plot settings ############################################################
+
+plotEveryN    = 100         # draw every plotEveryN'th cycle
+skipFirstN    = 0       # do not process the first skipFirstN cycles
+savePlot      = False      # save velocity norm and x velocity plot
+liveUpdate    = True      # show the process of the simulation (slow)
+saveVTK       = False       # save the vtk files
+prefix        = 'cyl_Re{0}_size{1}'.format(Re, size)      # naming prefix for saved files
+outputFolder  = './out'    # folder to save the output to
+workingFolder = os.getcwd()
+
+
+###### Flow definition #########################################################
+maxIterations = 200000  # Total number of time iterations.
+
+# Number of Cells
+ny = 41*factor + 2 # for boundary
+nx = 220*factor + 2 # for boundary
+
+# Highest index in each direction
+nxl = nx-1
+nyl = ny-1
+
+# populations
+q  = 9
+
+# Coordinates of the cylinder.
+cx = 20*factor + 1 # +1 for boundary
+cy = 21*factor + 1
+r = size/2
+
+# Velocity in lattice units.
+uLB  = 0.04
+uLBAverage = 2./3.*uLB # according to schaefer turek 2D-2
+nulb = uLBAverage*size/Re
+
+# Relaxation parameter
+omega = 1.0 / (3.*nulb+0.5)
+
+###### Plot preparations ############################################################
+
+# quick and dirty way to create output directory
+if not os.path.isdir(outputFolder):
+    try:
+        os.makedirs(outputFolder)
+    except OSError:
+        pass
+
+# Define the Grid for vtk output
+gridX = arange(0, nx, dtype='float64')
+gridY = arange(0, ny, dtype='float64')
+gridZ = arange(0, 1, dtype='float64')
+grid  = gridX, gridY, gridZ
+
+# Set velocity to 0 on z-axis
+velocityZ = zeros((nx, ny, 1))
+
+###### Setup ##################################################################
+
+# cylindrical obstacle
+obstacle       = fromfunction(lambda    x, y: (x-cx)**2+(y-cy)**2 < r**2,  (nx, ny))
+obstacleBounds, dragBoundStencil, liftBoundStencil, completeBoundStencil = obstacleAttack(obstacle)
+
+noSlipBoundary = fromfunction(lambda x, y: logical_or((y == 0), (y == ny)), (nx, ny))
+
+
+# velocity inlet for schaefer turek
+velIn = fromfunction(lambda d, x, y: (1-d)*4*uLB*y*(nyl-y)/(nyl**2),  (2, nx, ny-2))
+vel = zeros((2,nx,ny));
+vel[:,:,1:ny-1] = velIn
+
+# initial particle distributions
+feq   = equilibrium(1.0, vel)
+fin   = feq.copy()
+fpost = feq.copy()  # post collision distributions
+
+
+# interactive mode (execute code while showing figures)
+if ( liveUpdate | savePlot ):
+    pyplot.ion()
+    fig, ax = pyplot.subplots(1)
+
+os.chdir(outputFolder)
+
+###### Main time loop ##########################################################
+for time in range(maxIterations):
+    # bounce back distributions at obstacle
+    for i in range(q):
+        fin[i, obstacle] = fin[noslip[i], obstacle]
+    # and walls
+    for i in range(q):
+        fin[i, noSlipBoundary] = fin[noslip[i], noSlipBoundary]
+
+    # Right Wall: Produce zero pressure gradient for the outflow
+    fin[iLeft, -1, :] = fin[iLeft, -2, :]
+
+    # Calculate macroscopic density and velocity
+    (rho, u) = getMacroValues(fin)
+
+    # Left wall: compute density from known populations.
+    u[:, 0, :] = vel[:, 0, :]
+    rho[0, :] = 1./(1.-u[0, 0, :]) * (sumPopulations(fin[iCentV, 0, :])+2.*sumPopulations(fin[iLeft, 0, :]))
+
+    feq = equilibrium(rho, u)
+
+    # complete the left wall treatement wrt Yu 2002
+    fin[iRight, 0, :] = feq[iLeft, 0, :] + (feq[iRight, 0, :] - fin[iLeft, 0, :])
+
+    # Collision step.
+    #fpost = BGKCollide(fin, feq, omega)
+    fpost[:,1:nx-1,1:ny-1] = cumulantCollide(fin[:,1:nx-1,1:ny-1], rho[1:nx-1,1:ny-1], u[:,1:nx-1,1:ny-1], omega)
+
+    # Streaming step
+    fin = stream(fpost)
+
+    # Visualization
+    if ( (time % plotEveryN == 0) & (liveUpdate | saveVTK | savePlot) & (time > skipFirstN) ):
+        # Here, distributions are streamed into the obstacle -> compute drag and lift
+        scaling = 2/(sumPopulations(fin)*uLBAverage*uLBAverage*size)
+        scaledFin = fin.copy()
+        # just scale the populations which are used
+        for i in range(9):
+            scaledFin[i, completeBoundStencil] = scaledFin[i, completeBoundStencil]*scaling[completeBoundStencil]
+
+
+        dragCoeff = drag(scaledFin, obstacleBounds)
+        liftCoeff = lift(scaledFin, obstacleBounds)
+
+        if ( liveUpdate | savePlot ):
+            ax.clear()
+            ax.imshow(sqrt(u[0]**2+u[1]**2).transpose(),  cmap=cm.afmhot, vmin=0., vmax=0.1)
+            ax.set_title('velocity norm')
+
+            textstr = '$\mathrm{drag}= %.4f$\n$\mathrm{lift}= %.4f$' % (dragCoeff, liftCoeff)
+            props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+            ax.text(0.95, 0.95,
+                    textstr,
+                    horizontalalignment='right', verticalalignment='top',
+                    transform=ax.transAxes, fontsize=14,
+                    bbox=props)
+
+        if ( liveUpdate ):
+            pyplot.draw()
+        if ( saveVTK ):
+            # convert velocity and density to 3d arrays
+            printVel = reshape(u, (2, nx, ny, 1))
+            printRho = reshape(rho, (nx, ny, 1))
+
+            velocity = (printVel[0, :, :, :], printVel[1, :, :, :], velocityZ)
+            saveNumber = str(time/plotEveryN).zfill(4)
+
+            saveToVTK(velocity, printRho, prefix, saveNumber, grid)
+        if ( savePlot ):
+            pyplot.savefig(prefix + "." + str(time/plotEveryN).zfill(4) + ".png")
+
+os.chdir(workingFolder)
