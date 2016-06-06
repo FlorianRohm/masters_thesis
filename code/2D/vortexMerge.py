e0dbd0d58ee08115ef73aca47b28ce00591a09c3
Florian Rohm <rohm.florian@googlemail.com>
2016-06-06 18:32:40 +0200

    merge analysis

--- code/2D/vortexMerge.py
+++ code/2D/vortexMerge.py
@@ -29,7 +29,11 @@ from auxiliary.stream import stream
 from auxiliary.LBMHelpers import clamp, getMacroValues, sumPopulations, equilibrium, noslip, iLeft, iCentV, iRight, iTop, iCentH, iBot
 from auxiliary.boundaryConditions import YuLeft
 from auxiliary.obstacle import obstacleAttack, drag, lift
-from random import random
+from random import random, triangular, seed
+from numpy.fft import fft2
+from auxiliary.vorticity import vorticity
+seed(1503)
+
 import sys, getopt
 import os
 import datetime
@@ -37,14 +41,14 @@ import datetime
 ###### Parsing ############################################################
 
 Re = 100
-size = 100
+size = 128
 collisionFunction = BGKCollide
 collStr = "srt"
 try:
   opts, args = getopt.getopt(sys.argv[1:],"hcbr:s:",)
 except getopt.GetoptError:
     print "parse error"
-        print 'test.py -s <size of grid> -r <reynolds number> -c <use cumulant collision> '
+    print 'test.py -s <size of grid> -r <reynolds number> -c <use cumulant collision> '
     sys.exit(2)
 for opt, arg in opts:
     if opt == '-h':
@@ -58,7 +62,7 @@ for opt, arg in opts:
         collisionFunction = cumulantCollideAllInOne
         collStr = "cumulant"
 
-plotEveryN = 150
+plotEveryN = 100
 print 'Begin of calculation with {0} collision with Re={1} and size {2}'.format(collStr,Re,size)
 startTime = datetime.datetime.now()
 factor = size/10.
@@ -67,15 +71,16 @@ factor = size/10.
 ###### Plot settings ############################################################
 
 skipFirstN  = 0       # initial conditions already quite interesting
-savePlot      = False      # save velocity norm and x velocity plot
+savePlot      = True      # save velocity norm and x velocity plot
 liveUpdate    = True      # show the process of the simulation (slow)
+analysis      = False       # calculate and write enstrophy
 saveVTK       = False       # write out drag and lift
 prefix        = '{0}_Re{1}_size{2}'.format(collStr, Re, size)      # naming prefix for saved files
 outputFolder  = './out/vortexMerge'    # folder to save the outputFile to
 workingFolder = os.getcwd()
 
 ###### Flow definition #########################################################
-maxIterations = size*200  # Total number of time iterations.
+maxIterations = size*4000  # Total number of time iterations.
 
 # Number of Cells
 ny = size
@@ -130,22 +135,23 @@ print "Building vortices"
 vel = zeros((2, nx, ny))
 for x in range(nx):
     for y in range(ny):
-        for xoff in range(3):
-            xoff = xoff-1;
-            for yoff in range(3):
-                yoff = yoff-1;
-                vel[:,x,y] = vel[:,x,y] \
-                + rankinVortex(x-c1x + xoff*nx, y-c1y+ xoff*ny, diameter/2.) \
-                + rankinVortex(x-c2x + xoff*nx, y-c2y+ xoff*ny, diameter/2.) \
+        vel[:,x,y] = vel[:,x,y] \
+        + rankinVortex(x-c1x, y-c1y, diameter/2.) \
+        + rankinVortex(x-c2x, y-c2y, diameter/2.) \
 
 vel = vel*uLB
 print "Building vortices complete"
+avgVelX = mean(vel[0,:,:])
+avgVelY = mean(vel[1,:,:])
 
 # initial particle distributions
-feq   = equilibrium(1.0, vel)
+feq   = equilibrium(1.0, vel.reshape((2,nx*ny))).reshape((9,nx,ny))
 fin   = feq.copy()
 fpost = feq.copy()  # post collision distributions
 
+fluidDomain = full((nx,ny), True, dtype=bool)
+clipfft = 30
+fftDomain = fluidDomain[nx/2-clipfft:nx/2+clipfft, ny/2-clipfft:ny/2+clipfft]
 
 # interactive mode (execute code while showing figures)
 if ( liveUpdate | savePlot ):
@@ -154,6 +160,8 @@ if ( liveUpdate | savePlot ):
     ax.clear()
     ax.imshow(sqrt(vel[0]**2+vel[1]**2).transpose(),  cmap=cm.afmhot, vmin=0., vmax=0.05)
 
+if analysis:
+    outputFile = open(prefix, 'w')
 
 ###### Main time loop ##########################################################
 for time in range(maxIterations):
@@ -161,16 +169,30 @@ for time in range(maxIterations):
     (rho, u) = getMacroValues(fin)
 
     # Collision step.
-    fpost = collisionFunction(fin, rho, u, omega )
+    fpost[:,fluidDomain] = collisionFunction(fin[:,fluidDomain], rho[fluidDomain], u[:,fluidDomain], omega )
 
     # Streaming step
     fin = stream(fpost)
 
     # Visualization
-    if ( (time % plotEveryN == 0) & (liveUpdate | saveVTK | savePlot) & (time > skipFirstN) ):
+    if ( (time % plotEveryN == 0) & (liveUpdate | saveVTK | savePlot | analysis) & (time > skipFirstN) ):
+        vort = vorticity(u);
+        meanEnstrophy = mean(vort**2)/2.
+        turbulentKineticEnergy = mean((u[0]-avgVelX)**2 + (u[1]-avgVelY)**2)/2.
+
+        if (analysis):
+            outputFile.write("{0},{1}\n".format(meanEnstrophy, turbulentKineticEnergy))
+
         if ( liveUpdate | savePlot ):
             ax.clear()
-            ax.imshow(sqrt(u[0]**2+u[1]**2).transpose(),  cmap=cm.afmhot, vmin=0., vmax=0.05)
+            #vel2 = u[0]**2+u[1]**2
+            #vel2 = vel2.transpose();
+            #fftVel = fft2(vel2)
+            #fftVel = roll(roll(abs(fftVel),nx/2, axis=0), ny/2, axis=1)
+            #fftVel = fftVel[fftDomain].reshape((2*clipfft,2*clipfft))
+
+            ax.imshow(vort,  cmap=cm.gray, vmin=-0.005, vmax=0.005 )
+            #ax.imshow(sqrt(u[0]**2+u[1]**2).transpose(),  cmap=cm.afmhot, vmin=0., vmax=0.05)
 
         if ( liveUpdate ):
             pyplot.draw()
@@ -188,9 +210,11 @@ for time in range(maxIterations):
 
 endTime = datetime.datetime.now()
 deltaTime = endTime - startTime
-timeFile = open("{0}_time".format(prefix),"w")
-timeFile.write("{0}".format(deltaTime.total_seconds()));
-timeFile.write("\n");
-timeFile.close()
+if analysis:
+    timeFile = open("{0}_time".format(prefix),"w")
+    timeFile.write("{0},{1}".format(deltaTime.total_seconds(), sum(fluidDomain)));
+    timeFile.write("\n");
+    timeFile.close()
+    outputFile.close()
 os.chdir(workingFolder)
 print 'End of calculation with {0} collision with Re={1} and size {2}.\n Elapsed time: {3}'.format(collStr,Re,size,deltaTime.total_seconds())
